87885d1a3bd84cb07f7403f4a5a487c6785e6452
Koenraad Pecceu <koenraad.pecceu@gmail.com>
2024-04-13 12:05:25 +0200

    Change comments

--- Contacts/frmContacts.cs
+++ Contacts/frmContacts.cs
@@ -6,9 +6,9 @@ namespace Contacts
 {
     public partial class frmContacts : Form
     {
-        //globals
-        //path = pad + bestandsnaam van geopend bestand
-        //lines = verzameling van alle lijnen uit tekstbestand
+        // globals
+        // fullPath = pad + bestandsnaam van geopend bestand
+        // linesFromFile = verzameling van alle lijnen uit tekstbestand
         string fullPath;
         string[] linesFromFile;
 
@@ -17,38 +17,38 @@ namespace Contacts
             InitializeComponent();
         }
 
-        //methode om bestand in te lezen
-        //en personen toe te voegen aan dropdownlist
+        // methode om bestand in te lezen
+        // en personen toe te voegen aan dropdownlist
         private void AddPeople()
         {
-            //geopend bestand koppelen aan variabele fullPath
+            // geopend bestand koppelen aan variabele fullPath
 
             
-            //alle regels uit tekstbestand in array linesFromFile stoppen
+            // alle regels uit tekstbestand in array linesFromFile stoppen
             
 
-            //lines doorlopen en ieder lijntje splitsen
-            //daarna personen aanmaken en waarden toekennen
+            // lines doorlopen en ieder lijntje splitsen
+            // daarna personen aanmaken en waarden toekennen
 
             
         }
 
-        //methode om persoon aan te passen
-        //en alles opnieuw te bewaren in het geopende bestand
+        // methode om persoon aan te passen
+        // en alles opnieuw te bewaren in het geopende bestand
         private void UpdateFile()
         {
-            //geselecteerde persoon omzetten naar Person
+            // geselecteerde persoon omzetten naar Person
             
 
-            //alle waarden van p opnieuw invullen
-            //op basis van waarden die in textboxes staan
+            // alle waarden van p opnieuw invullen
+            // op basis van waarden die in textboxes staan
            
 
-            //lijn van persoon in lines array vervangen door
-            //nieuwe waarden
+            // lijn van persoon in lines array vervangen door
+            // nieuwe waarden
             
 
-            //alle lijnen opnieuw naar tekstbestand schrijven
+            // alle lijnen opnieuw naar tekstbestand schrijven
             
 
             //persoon in combobox ook aanpassen met nieuwe waarden
@@ -58,7 +58,7 @@ namespace Contacts
 
         private void btnOpen_Click(object sender, EventArgs e)
         {
-            //standaardmap instellen indien beschikbaar
+            // standaardmap instellen indien beschikbaar
 
             string defaultPath = @"C:\contacten\";
             if (Directory.Exists(defaultPath))
@@ -66,32 +66,33 @@ namespace Contacts
                 ofd.InitialDirectory = defaultPath;
             }
             
-            //dialoogvenster voor bestand openen starten
+            // dialoogvenster voor bestand openen starten
 
             ofd.ShowDialog();
         }
 
         private void ofd_FileOk(object sender, System.ComponentModel.CancelEventArgs e)
         {
-            //AddPeople methode uitvoeren om alle namen in de combobox te plaatsen
+            // AddPeople methode uitvoeren om alle namen in de combobox te plaatsen
             AddPeople();
-            //eerste item uit de combobox selecteren
-            cbPeople.SelectedIndex = 0;
+            // eerste item uit de combobox selecteren
+            
+
         }
 
         private void cbPeople_SelectedIndexChanged(object sender, EventArgs e)
         {
-            //geselecteerd item 'omzetten' (casten) naar Person
+            // geselecteerd item 'omzetten' (casten) naar Person
           
 
-            //tekstvakken invullen met gegevens van persoon
+            // tekstvakken invullen met gegevens van persoon
             
 
         }
 
         private void btnSave_Click(object sender, EventArgs e)
         {
-            //UpdateFile methode uitvoeren om aangepaste persoon te bewaren
+            // UpdateFile methode uitvoeren om aangepaste persoon te bewaren
             UpdateFile();
         }
     }
